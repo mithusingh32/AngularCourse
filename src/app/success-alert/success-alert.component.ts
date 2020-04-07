1d0885d3486b4d001272fdb8d838a671377b143d
Sukhmeet Khalar <mithusingh32@gmail.com>
2020-04-06 20:20:37 -0400

    Module2_Ex1

---
+++ src/app/success-alert/success-alert.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-success-alert',
+  template: '<h3>Success!</h3>',
+  styles: ['h3 { color: green; }']
+})
+export class SuccessAlertComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
