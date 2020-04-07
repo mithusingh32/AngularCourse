d5d91fbb4bfd14f66be005d3c3149c5e50e16a5f
Sukhmeet Khalar <mithusingh32@gmail.com>
2020-04-07 16:24:42 -0400

    Module Exercise 2

---
+++ src/app/servers/servers.component.ts
@@ -0,0 +1,53 @@
+import { Component, OnInit } from '@angular/core';
+
+// IDs are not supported as selectors in angular
+@Component({
+    // selector: '[app-servers]', // Property selecctor
+    // selector: '.app-servers',  // Class selector
+    selector: 'app-servers',      // Custom element selector
+    templateUrl: './servers.component.html',
+    styleUrls: ['./servers.component.css']
+})
+export class ServersComponent implements OnInit {
+    allowNewServer = false;
+    serverCreationStatus: string = "No Server Created";
+    serverName = "";
+    userName: string;
+    inputUserName: string; 
+    hideParagraph = true;
+
+    constructor() {
+        setTimeout(() => {
+            this.allowNewServer = true;
+        }, 2000);
+    }
+
+    ngOnInit() {
+    }
+
+    onCreateServer() {
+        this.serverCreationStatus = "Server " + this.serverName + " was created";
+    }
+
+    onUpdateServerName(event: Event) {
+        console.log(event);
+        this.serverName = (<HTMLInputElement>event.target).value; // Need to cast it so Typescript knows what we're trying to get. Event itself doesnt have a value, so casting it to <HTMLInputElement> gives it that knowledge  
+    }
+/*Exercise 2*/
+    onInput(event: Event)
+    {
+        this.userName = (<HTMLInputElement>event.target).value
+    }
+
+    isInputEmpty()
+    {
+        return (this.userName == "" || this.userName == null);
+    }
+
+    onClicked()
+    {
+        this.hideParagraph = false; 
+        this.inputUserName = this.userName;
+        this.userName = "";
+    }
+}
