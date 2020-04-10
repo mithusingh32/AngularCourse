efe3ca69960395e2db3bff2419bec4880ca559d4
Sukhmeet Khalar <mithusingh32@gmail.com>
2020-04-10 13:02:47 -0400

    Course Project Setup

---
+++ src/app/shared/ingredient.model.ts
@@ -0,0 +1,3 @@
+export class Ingredient {
+    constructor(public name: string, public amount: number) {} // This will create member member variables and set then based on the constructors inputs
+}
