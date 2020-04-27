a2a76ee9dd730f330a516a7d9c721452e1ef37cf
Sukhmeet Khalar <mithusingh32@gmail.com>
2020-04-26 22:00:12 -0400

    Template Drived Form implemented
    - This is much easier than using FormGroup and FormBuilder in JS. You
    should try to use this deisgn when possible.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  submitted: boolean = false;
+  @ViewChild('formInput') formInput: NgForm;
 
+  userData = {
+    email: "",
+    subscription: "",
+    password: "", 
+
+  }
+
+  onSubmit() {
+    console.log(this.formInput.value);
+    this.userData.email = this.formInput.value.userEmail;
+    this.userData.subscription = this.formInput.value.subType;
+    this.userData.password = this.formInput.value.password;
+
+    this.submitted = true;
+
+    this.formInput.reset();
+  }
 }
